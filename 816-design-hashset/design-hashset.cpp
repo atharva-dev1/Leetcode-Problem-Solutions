d1370f48860359413dcd324e8e46734a0a6f16d5
Atharva Sharma <2431560atharva@gmail.com>
2026-02-01 23:33:14 +0530

    Time: 5 ms (93.68%) | Memory: 48.1 MB (76.47%) - LeetSync

---
+++ 816-design-hashset/design-hashset.cpp
@@ -0,0 +1,28 @@
+class MyHashSet {
+public:
+    MyHashSet() { 
+    }
+     unordered_set<int>s;
+    void add(int key) {
+        s.insert(key);
+    }
+    
+    void remove(int key) {
+        s.erase(key);
+    }
+    
+    bool contains(int key) {
+      if(s.find(key) == s.end()) 
+      return false;
+      else
+      return true;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
