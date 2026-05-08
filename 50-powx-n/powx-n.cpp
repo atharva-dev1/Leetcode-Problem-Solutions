3ed306328c0986addadab8e81cbdfba556d54cad
Atharva Sharma <2431560atharva@gmail.com>
2026-05-08 18:03:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.26%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+        if (n == 0 || x == 1) return 1.0;
+        if(x == 0) return 0.0;
      long bF = n;
      if (n < 0){
         x = 1/x;
