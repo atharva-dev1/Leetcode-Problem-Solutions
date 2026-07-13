e46ad43460d9b90d42558c569f9ab63d1498c980
Atharva Sharma <2431560atharva@gmail.com>
2026-07-13 14:33:29 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (74.12%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -12,15 +12,26 @@
 class Solution {
 public:
 int count = 0 ;
-    int countNodes(TreeNode* root) {
-        Inorder(root);
+    int countNodes(TreeNode* root) {       // Aprroach 1
+        return countNode(root);
         return count;
     }
-    void Inorder(TreeNode * root){
-        if (root == NULL)
-        return;
-            Inorder(root ->left);
-            count++;
-            Inorder(root -> right);
-        }
+    // void Inorder(TreeNode * root){
+    //     if (root == NULL)
+    //     return;
+    //         Inorder(root ->left);
+    //         count++;
+    //         Inorder(root -> right);
+    //     }
+
+    int countNode(TreeNode *root){     //approach 2 
+        if(root == NULL)
+        return 0;
+
+        int left = countNode(root-> left);
+        int right = countNode(root -> right);
+        return 1 + left + right;
+    }
+
+
 };
